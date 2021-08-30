cb71e8ff30ae1e15aa73c17d098ff91a3250e3eb
Nedu <nedumrobert@gmail.com>
2021-08-30 15:52:46 -0400

    feat: user model

--- user/user.go
+++ user/user.go
@@ -24,7 +24,22 @@ type UserRole struct {
 }
 
 type UserSettings struct {
-	role UserRole
+	role []UserRole
+}
+
+type UserEmailVerification struct {
+	verified   bool
+	token      string
+	expired_at primitive.Timestamp
+}
+
+type UserPasswordReset struct {
+	ID         primitive.ObjectID `bson:"_id"`
+	ipaddress  string
+	token      string
+	expired_at string `bson:"expired_at"`
+	updated_at string `bson:"updated_at"`
+	created_at string `bson:"created_at"`
 }
 
 type Status int
@@ -36,17 +51,20 @@ const (
 )
 
 type User struct {
-	ID         primitive.ObjectID `bson:"_id"`
-	first_name string
-	last_name  string
-	email      string
-	password   string
-	status     Status
-	company    string
-	settings   UserSettings
-	timezone   string
-	created_at time.Time
-	updated_at time.Time
-	deleted_at time.Time
-	workspaces []UserWorkspace
+	ID                 primitive.ObjectID `bson:"_id"`
+	first_name         string             `json:"first_name" validate:"required,min=2,max=100"`
+	last_name          string             `json:"last_name" validate:"required,min=2,max=100"`
+	email              string             `json:"email" validate:"email,required"`
+	password           string             `json:"Password" validate:"required,min=6""`
+	phone              string             `json:"phone" validate:"required"`
+	status             Status
+	company            string
+	settings           UserSettings
+	timezone           string
+	created_at         time.Time `bson:"created_at"`
+	updated_at         time.Time `bson:"updated_at"`
+	deleted_at         time.Time `bson:"deleted_at"`
+	workspaces         []UserWorkspace
+	email_verification UserEmailVerification
+	password_resets    []UserPasswordReset
 }
