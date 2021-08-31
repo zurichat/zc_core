d671667ad2c1f6c6546ae03882886c33fe3e476f
Abiola Fasanya <hezekiahfasanya@gmail.com>
2021-08-31 08:02:32 +0100

    Create plugins.go

---
+++ marketplace/plugins.go
@@ -0,0 +1,41 @@
+package marketplace
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"zuri.chat/zccore/utils"
+)
+
+func Plugins(response http.ResponseWriter, request *http.Request) {
+	response.WriteHeader(http.StatusOK)
+	var pluguns []Plugins
+
+	response.Header().Set("Content-Type", "application/json")
+
+	DbName, CollectionName := utils.Env("DB_NAME"), "plugins"
+	collection, err := utils.GetMongoDbCollection(DbName, CollectionName)
+	if err != nil {
+		utils.GetError(err, 500, response)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		utils.GetError(err, 400, response)
+		return
+	}
+
+	defer cursor.Close(ctx)
+
+	plugins = make([]Plugins, 0)
+	for cursor.Next(ctx) {
+		var plugin Plugins
+		cursor.Decode(&plugin)
+		apps = append(plugins, plugin)
+	}
+
+	json.NewEncoder(response).Encode(plugins)
+}
