6f84e47620db18f9f8fba1cdf6c8fb82503533c5
Helewud <helewud@gmail.com>
2021-09-03 04:16:53 +0100

    added a helper function to update profile status in a workspace

--- user/user.go
+++ user/user.go
@@ -16,7 +16,7 @@ func Create(response http.ResponseWriter, request *http.Request) {
 	user_collection := "users"
 
 	var user User
-	
+
 	err := utils.ParseJsonFromRequest(request, &user)
 	if err != nil {
 		utils.GetError(err, http.StatusUnprocessableEntity, response)
@@ -27,7 +27,7 @@ func Create(response http.ResponseWriter, request *http.Request) {
 		utils.GetError(errors.New("email address is not valid"), http.StatusBadRequest, response)
 		return
 	}
-	
+
 	// confirm if user_email exists
 	result, _ := utils.GetMongoDbDoc(user_collection, bson.M{"email": user.Email})
 	if result != nil {
@@ -35,13 +35,13 @@ func Create(response http.ResponseWriter, request *http.Request) {
 		utils.GetError(errors.New("operation failed"), http.StatusBadRequest, response)
 		return
 	}
-		
+
 	user.CreatedAt = time.Now()
 
 	detail, _ := utils.StructToMap(user)
 
 	res, err := utils.CreateMongoDbDoc(user_collection, detail)
-	
+
 	if err != nil {
 		utils.GetError(err, http.StatusInternalServerError, response)
 		return
@@ -85,3 +85,22 @@ func Create(response http.ResponseWriter, request *http.Request) {
 // func CreateUserProfile(ctx context.Context, uw *UserWorkspace) error {
 // 	return nil
 // }
+
+// helper function perform update workspace profile status
+func SetWorkspaceProfileStatus(ctx context.Context, id string, status string) ([]*WorkSpaceProfile, error) {
+	workspaceProfile := []*WorkSpaceProfile{}
+
+	collectionName := "userWorkSpace"
+	objID, _ := primitive.ObjectIDFromHex(id)
+	collection := utils.GetCollection(collectionName)
+	filter := bson.M{"_id": objID}
+	update := bson.M{"$set": bson.M{"status": status}}
+
+	res := collection.FindOneAndUpdate(ctx, filter, update)
+	if err := res.Decode(&workspaceProfile); err != nil {
+		return nil, err
+	}
+
+	return workspaceProfile, nil
+
+}
