f13c0a91f6993b123a6cd58458a1e38b182fc5e9
Jotham-Ntekim <jothamweb@gmail.com>
2021-09-03 12:55:52 +0100

    updated user/user.go

--- user/user.go
+++ user/user.go
@@ -57,13 +57,17 @@ func DeleteUser(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	userId := params["user_id"]
 
-
 	save, err := utils.DeleteOneMongoDoc("users", userId)
 
 	if err != nil {
 		utils.GetError(err, http.StatusInternalServerError, w)
 		return
 	}
+	if response.DeletedCount == 0 {
+		utils.GetError(errors.New("operation failed"), http.StatusInternalServerError, w)
+		return
+	}
+
 	utils.GetSuccess("User Deleted Succesfully", save, w)
 }
 
