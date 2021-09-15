ec5bae8c10301cb6ae8c3f10ce23946501217165
Oyewole Samuel A <oyewoleabayomi@gmail.com>
2021-09-15 14:28:40 +0100

    create pwreset methods

--- auth/pwreset.go
+++ auth/pwreset.go
@@ -1,1 +1,10 @@
 package auth
+
+import "net/http"
+
+
+func (au *AuthHandler) VerifyMail(w http.ResponseWriter, r *http.Request) {}
+
+func (au *AuthHandler) VerifyPasswordReset(w http.ResponseWriter, r *http.Request){}
+
+func (au *AuthHandler) GeneratePassResetCode(w http.ResponseWriter, r *http.Request){}
