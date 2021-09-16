09b15b68f098a975d7cede620e3c87266b5182e9
Oyewole Samuel A <oyewoleabayomi@gmail.com>
2021-09-16 10:21:57 +0100

    update session max age

--- auth/login.go
+++ auth/login.go
@@ -52,7 +52,7 @@ func (au *AuthHandler) LoginIn(response http.ResponseWriter, request *http.Reque
 		utils.GetError(InvalidCredentials, http.StatusBadRequest, response)
 		return
 	}
-	store := NewMongoStore(utils.GetCollection(session_collection), SESSION_MAX_AGE, true, []byte(secretKey))
+	store := NewMongoStore(utils.GetCollection(session_collection), au.configs.SessionMaxAge, true, []byte(secretKey))
 	var session, e = store.Get(request, sessionKey)
 	if e != nil {
 		msg := fmt.Errorf("%s", e.Error())
@@ -101,7 +101,7 @@ func (au *AuthHandler) LoginIn(response http.ResponseWriter, request *http.Reque
 func (au *AuthHandler) LogOutUser(w http.ResponseWriter, r *http.Request) {
 	store := NewMongoStore(
 		utils.GetCollection(session_collection),
-		SESSION_MAX_AGE,
+		au.configs.SessionMaxAge,
 		true,
 		[]byte(secretKey),
 	)
