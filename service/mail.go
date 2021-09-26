706571f95b26ec1265c6491dfdc89490ce1aadec
Oyewole Samuel A <oyewoleabayomi@gmail.com>
2021-09-26 19:27:08 +0100

    remove comment

--- service/mail.go
+++ service/mail.go
@@ -31,15 +31,6 @@ const (
 	WorkspaceInvite
 )
 
-// type MailData struct {
-// 	Username   string
-// 	Code       string
-// 	OrgName    string
-// 	InviteLink string
-// 	ZuriLogo   string
-// 	Image2     string
-// }
-
 type Mail struct {
 	to      []string
 	subject string
