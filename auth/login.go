7bacc3d6f7f9bc35a95e3612ddf3d23b60ea848c
Oyewole Samuel A <oyewoleabayomi@gmail.com>
2021-10-07 05:43:27 +0100

    lint auth module

--- auth/login.go
+++ auth/login.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"net/url"
 	"strings"
 	"time"
 
@@ -19,47 +20,51 @@ import (
 	"zuri.chat/zccore/utils"
 )
 
+type key int
+
 const (
-	enckry             = "piqenpdan9n-94n49-e-9ad-aononoon"
-	session_collection = "session_store"
-	user_collection    = "users"
+	sessionCollection     = "session_store"
+	userCollection        = "users"
+	UserContext       key = iota
 )
 
 var (
-	validate            = validator.New()
-	UserNotFound        = errors.New("User not found, confirm and try again!")
-	InvalidCredentials  = errors.New("Invalid login credentials, confirm and try again")
-	AccountConfirmError = errors.New("Your account is not verified, kindly check your email for verification code.")
-	AccessExpired		= errors.New("error fetching user info, access token expired, kindly login again")
+	validate               = validator.New()
+	ErrUserNotFound        = errors.New("user not found, confirm and try again")
+	ErrInvalidCredentials  = errors.New("invalid login credentials, confirm and try again")
+	ErrAccountConfirmError = errors.New("your account is not verified, kindly check your email for verification code")
+	ErrAccessExpired       = errors.New("error fetching user info, access token expired, kindly login again")
 )
 
-func (au *AuthHandler) GetAuthToken(user *user.User, sess *sessions.Session) (*Token, error) {
+func (au *AuthHandler) GetAuthToken(u *user.User, sess *sessions.Session) (*Token, error) {
 	retoken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"session_name": Resptoken.SessionName,
 		"cookie":       Resptoken.Cookie,
 		"options":      Resptoken.Options,
-		"id":           Resptoken.Id,
-		"email":        user.Email,
+		"id":           Resptoken.ID,
+		"email":        u.Email,
 	})
 
 	tokenString, err := retoken.SignedString([]byte(au.configs.HmacSampleSecret))
-	if err != nil { return nil, err }
+	if err != nil {
+		return nil, err
+	}
 
 	resp := &Token{
 		SessionID: sess.ID,
 		User: UserResponse{
-			ID:        user.ID,
-			FirstName: user.FirstName,
-			LastName:  user.LastName,
-			Email:     user.Email,
-			Phone:     user.Phone,
-			Timezone:  user.Timezone,
-			CreatedAt: user.CreatedAt,
-			UpdatedAt: user.UpdatedAt,
+			ID:        u.ID,
+			FirstName: u.FirstName,
+			LastName:  u.LastName,
+			Email:     u.Email,
+			Phone:     u.Phone,
+			Timezone:  u.Timezone,
+			CreatedAt: u.CreatedAt,
+			UpdatedAt: u.UpdatedAt,
 			Token:     tokenString,
 		},
 	}
-	
+
 	return resp, nil
 }
 
@@ -71,45 +76,50 @@ func (au *AuthHandler) LoginIn(response http.ResponseWriter, request *http.Reque
 		utils.GetError(err, http.StatusUnprocessableEntity, response)
 		return
 	}
+
 	if err := validate.Struct(creds); err != nil {
 		utils.GetError(err, http.StatusBadRequest, response)
 		return
 	}
 
-	user, err := FetchUserByEmail(bson.M{"email": strings.ToLower(creds.Email)})
+	vser, err := FetchUserByEmail(bson.M{"email": strings.ToLower(creds.Email)})
 	if err != nil {
-		utils.GetError(UserNotFound, http.StatusBadRequest, response)
+		utils.GetError(ErrUserNotFound, http.StatusBadRequest, response)
 		return
 	}
 	// check if user is verified
-	if user.IsVerified != true {
-		utils.GetError(AccountConfirmError, http.StatusBadRequest, response)
+	if vser.IsVerified {
+		utils.GetError(ErrAccountConfirmError, http.StatusBadRequest, response)
 		return
 	}
 
 	// check password
-	check := CheckPassword(creds.Password, user.Password)
+	check := CheckPassword(creds.Password, vser.Password)
 	if !check {
-		utils.GetError(InvalidCredentials, http.StatusBadRequest, response)
+		utils.GetError(ErrInvalidCredentials, http.StatusBadRequest, response)
 		return
 	}
-	store := NewMongoStore(utils.GetCollection(session_collection), au.configs.SessionMaxAge, true, []byte(au.configs.SecretKey))
+
+	store := NewMongoStore(utils.GetCollection(sessionCollection), au.configs.SessionMaxAge, true, []byte(au.configs.SecretKey))
+
 	var session, e = store.Get(request, au.configs.SessionKey)
 	if e != nil {
 		msg := fmt.Errorf("%s", e.Error())
 		utils.GetError(msg, http.StatusBadRequest, response)
+
 		return
 	}
 
 	// store session
-	session.Values["id"] = user.ID
-	session.Values["email"] = user.Email
+	session.Values["id"] = vser.ID
+	session.Values["email"] = vser.Email
 
 	if err = sessions.Save(request, response); err != nil {
 		fmt.Printf("Error saving session: %s", err)
 		return
 	}
-	resp, err := au.GetAuthToken(user, session)
+
+	resp, err := au.GetAuthToken(vser, session)
 
 	if err != nil {
 		utils.GetError(err, http.StatusBadRequest, response)
@@ -120,69 +130,68 @@ func (au *AuthHandler) LoginIn(response http.ResponseWriter, request *http.Reque
 }
 
 func (au *AuthHandler) LogOutUser(w http.ResponseWriter, r *http.Request) {
+	var (
+		session *sessions.Session
+		err     error
+		erro    error
+	)
+
 	store := NewMongoStore(
-		utils.GetCollection(session_collection),
+		utils.GetCollection(sessionCollection),
 		au.configs.SessionMaxAge,
 		true,
 		[]byte(au.configs.SecretKey),
 	)
-	var session *sessions.Session
-	var err error
-	session, err = store.Get(r, au.configs.SessionKey)
-	status, _, sessData := GetSessionDataFromToken(r, []byte(au.configs.HmacSampleSecret))
 
-	if err != nil && status == false {
-		utils.GetError(NotAuthorized, http.StatusUnauthorized, w)
+	session, err = store.Get(r, au.configs.SessionKey)
+	status, sessData, _ := GetSessionDataFromToken(r, []byte(au.configs.HmacSampleSecret))
+
+	if err != nil && status {
+		utils.GetError(ErrNotAuthorized, http.StatusUnauthorized, w)
 		return
 	}
-	var erro error
-	if status == true {
-		session, erro = NewS(store, sessData.Cookie, sessData.Id, sessData.Email, r, sessData.SessionName, sessData.Gothic)
-		// fmt.Println(session)
+
+	if status {
+		session, erro = NewS(store, sessData.Cookie, sessData.ID, sessData.Email, r, sessData.SessionName, sessData.Gothic)
 		if err != nil && erro != nil {
-			utils.GetError(NotAuthorized, http.StatusUnauthorized, w)
+			utils.GetError(ErrNotAuthorized, http.StatusUnauthorized, w)
 			return
 		}
 	}
 
-	if session.IsNew == true {
-		utils.GetError(NoAuthToken, http.StatusUnauthorized, w)
+	if session.IsNew {
+		utils.GetError(ErrNoAuthToken, http.StatusUnauthorized, w)
 		return
 	}
 
-	// fmt.Println(session)
 	session.Options.MaxAge = -1
 
 	if err = ClearSession(store, w, session); err != nil {
 		fmt.Printf("Error saving session: %s", err)
 		utils.GetError(fmt.Errorf("logout Failed"), http.StatusUnauthorized, w)
+
 		return
 	}
-	// if err = sessions.Save(r, w); err != nil {
-	// 	fmt.Printf("Error saving session: %s", err)
-	// 	utils.GetError(fmt.Errorf("Logout Failed"), http.StatusUnauthorized, w)
-	// 	return
-	// }
 
 	utils.GetSuccess("logout successful", map[string]interface{}{}, w)
 }
 
 func (au *AuthHandler) VerifyTokenHandler(response http.ResponseWriter, request *http.Request) {
 	// extract user id and email from context
-	loggedIn := request.Context().Value("user").(*AuthUser)
-	user, _ := FetchUserByEmail(bson.M{"email": strings.ToLower(loggedIn.Email)})
+	loggedIn, _ := request.Context().Value("user").(*AuthUser)
+	u, _ := FetchUserByEmail(bson.M{"email": strings.ToLower(loggedIn.Email)})
 
 	resp := &VerifiedTokenResponse{
 		true,
 		UserResponse{
-			ID:        user.ID,
-			FirstName: user.FirstName,
-			LastName:  user.LastName,
-			Email:     user.Email,
-			Phone:     user.Phone,
-			Timezone:  user.Timezone,
-			CreatedAt: user.CreatedAt,
-			UpdatedAt: user.UpdatedAt,
+			ID:        u.ID,
+			FirstName: u.FirstName,
+			LastName:  u.LastName,
+			Email:     u.Email,
+			Phone:     u.Phone,
+			Timezone:  u.Timezone,
+			CreatedAt: u.CreatedAt,
+			UpdatedAt: u.UpdatedAt,
 		},
 	}
 
@@ -190,56 +199,56 @@ func (au *AuthHandler) VerifyTokenHandler(response http.ResponseWriter, request 
 }
 
 func (au *AuthHandler) LogOutOtherSessions(w http.ResponseWriter, r *http.Request) {
+	var (
+		session *sessions.Session
+		err     error
+	)
 
 	store := NewMongoStore(
-		utils.GetCollection(session_collection),
+		utils.GetCollection(sessionCollection),
 		au.configs.SessionMaxAge,
 		true,
 		[]byte(au.configs.SecretKey),
 	)
-	var session *sessions.Session
-	var err error
 
 	// Get  current session
 	session, err = store.Get(r, au.configs.SessionKey)
-	status, _, sessData := GetSessionDataFromToken(r, []byte(au.configs.HmacSampleSecret))
+	status, sessData, _ := GetSessionDataFromToken(r, []byte(au.configs.HmacSampleSecret))
 
-	if err != nil && status == false {
-		utils.GetError(NotAuthorized, http.StatusUnauthorized, w)
+	if err != nil && status {
+		utils.GetError(ErrNotAuthorized, http.StatusUnauthorized, w)
 		return
 	}
 
 	// Handles token session
-	if status == true {
+	if status {
 		email := sessData.Email
-		user, err := FetchUserByEmail(bson.M{"email": email})
+		u, err := FetchUserByEmail(bson.M{"email": email})
+
 		if err != nil {
 			utils.GetError(err, http.StatusInternalServerError, w)
 		}
 
 		// Check that password is correct
-		check := CheckPassword(r.FormValue("password"), user.Password)
+		check := CheckPassword(r.FormValue("password"), u.Password)
 		if !check {
 			utils.GetError(errors.New("invalid password, confirm and try again"), http.StatusBadRequest, w)
 			return
 		}
 
 		// delete other sessions apart from current one
-		DeleteOtherSessions(user.ID, session.ID)
-
-		// Handles cookie sessions
+		DeleteOtherSessions(u.ID, session.ID)
 	} else {
-		// Get current user ID
-		id := session.Values["id"].(string)
+		id, _ := session.Values["id"].(string)
 
-		// Get user who has such ID
-		user, er := FetchUserByID(id)
+		// Get u who has such ID
+		u, er := FetchUserByID(id)
 		if er != nil {
 			utils.GetError(er, http.StatusInternalServerError, w)
 		}
 
 		// Check that password is correct
-		check := CheckPassword(r.FormValue("password"), user.Password)
+		check := CheckPassword(r.FormValue("password"), u.Password)
 		if !check {
 			utils.GetError(errors.New("invalid password, confirm and try again"), http.StatusBadRequest, w)
 			return
@@ -252,7 +261,7 @@ func (au *AuthHandler) LogOutOtherSessions(w http.ResponseWriter, r *http.Reques
 	utils.GetSuccess("successfully logged out of other sessions", nil, w)
 }
 
-func (au *AuthHandler) SocialAuth(w http.ResponseWriter, r *http.Request){
+func (au *AuthHandler) SocialAuth(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,access-control-allow-origin, access-control-allow-headers")
 
@@ -261,7 +270,7 @@ func (au *AuthHandler) SocialAuth(w http.ResponseWriter, r *http.Request){
 
 	// default providers
 	providers := map[string]string{
-		"google": au.configs.GoogleOAuthV3Url,
+		"google":   au.configs.GoogleOAuthV3Url,
 		"facebook": au.configs.FacebookOAuthUrl,
 	}
 
@@ -273,29 +282,32 @@ func (au *AuthHandler) SocialAuth(w http.ResponseWriter, r *http.Request){
 		Provider:    params["provider"],
 		AccessToken: params["access_token"],
 	}
-	
+
 	if err := validate.Struct(social); err != nil {
 		utils.GetError(err, http.StatusBadRequest, w)
 		return
 	}
 
-	providerUrl, ok := providers[strings.ToLower(social.Provider)]
+	providerURL, ok := providers[strings.ToLower(social.Provider)]
 	if !ok {
 		errMsg := fmt.Sprintf("Implementation error: %s does not exists!", social.Provider)
 		utils.GetError(errors.New(errMsg), http.StatusBadRequest, w)
+
 		return
 	}
 
-	url := strings.Replace(providerUrl, ":access_token", social.AccessToken, 1)
-	resp, err := http.Get(url)
+	reqURL, _ := url.Parse(strings.Replace(providerURL, ":access_token", social.AccessToken, 1))
+	resp, err := http.Get(reqURL.String())
+
 	if err != nil || resp.StatusCode != 200 {
-		utils.GetError(AccessExpired, http.StatusBadRequest, w)
+		utils.GetError(ErrAccessExpired, http.StatusBadRequest, w)
 		return
 	}
 	defer resp.Body.Close()
 
-	store := NewMongoStore(utils.GetCollection(session_collection), au.configs.SessionMaxAge, true, []byte(au.configs.SecretKey))
-	var session, e = store.Get(r, au.configs.SessionKey)
+	store := NewMongoStore(utils.GetCollection(sessionCollection), au.configs.SessionMaxAge, true, []byte(au.configs.SecretKey))
+	session, e := store.Get(r, au.configs.SessionKey)
+
 	if e != nil {
 		utils.GetError(e, http.StatusBadRequest, w)
 		return
@@ -304,61 +316,61 @@ func (au *AuthHandler) SocialAuth(w http.ResponseWriter, r *http.Request){
 	switch p := strings.ToLower(social.Provider); p {
 	case "google":
 		socialUser := struct {
-			ID				string		`json:"sub"`
-			Email			string		`json:"email"`
-			EmailVerified	string		`json:"email_verified"`
-			FirstName		string		`json:"given_name"`
-			LastName		string		`json:"family_name"`
-			Picture			string		`json:"picture"`
+			ID            string `json:"sub"`
+			Email         string `json:"email"`
+			EmailVerified string `json:"email_verified"`
+			FirstName     string `json:"given_name"`
+			LastName      string `json:"family_name"`
+			Picture       string `json:"picture"`
 		}{}
 
-		//check if user exists
+		//nolint:errcheck //CODEI8:
 		json.NewDecoder(resp.Body).Decode(&socialUser)
+
 		filter := bson.M{"$or": []bson.M{
 			{"email": socialUser.Email},
 			{"social.provider": p, "social.provider_id": socialUser.ID},
-		} }
+		}}
 
 		vser, err := FetchUserByEmail(filter)
 		if err != nil {
-			// user not found, create one
 			social := &user.Social{ID: socialUser.ID, Provider: p}
 			b := &user.User{
-				FirstName: socialUser.FirstName,
-				LastName: socialUser.LastName,
-				Email: socialUser.Email,
-				Password: "",
-				Deactivated: false,
-				IsVerified: true,
-				Social: social,
-				Timezone : "Africa/Lagos", // set default timezone
+				FirstName:     socialUser.FirstName,
+				LastName:      socialUser.LastName,
+				Email:         socialUser.Email,
+				Password:      "",
+				Deactivated:   false,
+				IsVerified:    true,
+				Social:        social,
+				Timezone:      "Africa/Lagos",                       // set default timezone
 				Organizations: []string{"614679ee1a5607b13c00bcb7"}, // set default org
-				CreatedAt: time.Now(),
+				CreatedAt:     time.Now(),
 			}
 			detail, _ := utils.StructToMap(b)
-			res, err := utils.CreateMongoDbDoc(user_collection, detail)
+			res, er := utils.CreateMongoDbDoc(userCollection, detail)
 
-			if err != nil {
-				utils.GetError(err, http.StatusInternalServerError, w)
+			if er != nil {
+				utils.GetError(er, http.StatusInternalServerError, w)
 				return
 			}
 
 			session.Values["id"] = res.InsertedID
-			session.Values["email"] = b.Email						
+			session.Values["email"] = b.Email
 		} else {
 			// update record
 			social := map[string]interface{}{
 				"provider_id": socialUser.ID,
-				"provider": p,
+				"provider":    p,
 			}
 
 			id, _ := primitive.ObjectIDFromHex(vser.ID)
 			filter := bson.M{"_id": id}
-			update := bson.M{"$set": bson.M{"social": social, "email": strings.ToLower(socialUser.Email) }}
-			
-			if _, err := utils.GetCollection(user_collection).UpdateOne(context.Background(), filter, update); err != nil {
-				utils.GetError(err, http.StatusInternalServerError, w)
-				return		
+			update := bson.M{"$set": bson.M{"social": social, "email": strings.ToLower(socialUser.Email)}}
+
+			if _, e := utils.GetCollection(userCollection).UpdateOne(context.Background(), filter, update); e != nil {
+				utils.GetError(e, http.StatusInternalServerError, w)
+				return
 			}
 
 			session.Values["id"] = vser.ID
@@ -375,15 +387,18 @@ func (au *AuthHandler) SocialAuth(w http.ResponseWriter, r *http.Request){
 			utils.GetError(err, http.StatusBadRequest, w)
 			return
 		}
-	
-		utils.GetSuccess("login successful", resp, w)	
-		return 	
+
+		utils.GetSuccess("login successful", resp, w)
+
+		return
 	case "facebook":
-		utils.GetError(errors.New("Facebook: Pending implementation, contact admin!"), http.StatusBadRequest, w)
+		utils.GetError(errors.New("facebook: pending implementation, contact admin"), http.StatusBadRequest, w)
+
 		return
 	default:
 		msg := fmt.Sprintf("Implementation error: %s does not exists!", p)
 		utils.GetError(errors.New(msg), http.StatusBadRequest, w)
+
 		return
 	}
 }
